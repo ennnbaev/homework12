9edd61a083ce462f807c633fcb7d51beaaca380c
Danil IenbaIev <ennnbaev@icloud.com>
2021-09-03 19:07:29 +0300

    Нет сообщений в кетч

--- src/com/company/Homework.java
+++ src/com/company/Homework.java
@@ -14,29 +14,32 @@ public class Homework {
     public static Employee createEmployee() throws IOException {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
+        while (true) {
         System.out.println("Введите имя работника");
         String firstName=reader.readLine();
         System.out.println("Введите фамилию работника");
         String lastName=reader.readLine();
         System.out.println("Введите размер зарплаты");
         String salary1=reader.readLine();
-        double salary=Double.parseDouble(salary1);
-        while (true) {
             try {
+                double  salary=Double.parseDouble(salary1);
                 return new Employee(firstName, lastName, salary);
             }
             catch (ImpossibleNameException exception) {
                 System.err.println("ФИО имеет цифры или не англ символы "+exception.input );
                 System.out.println("Фаимилия или имя написаны неверно попробуйте еще раз");
-                firstName=reader.readLine();
-                lastName=reader.readLine();
+//                firstName=reader.readLine();
+//                lastName=reader.readLine();
             }
             catch (TooSmallSalaryException exc){
             System.err.println("Введенное число отрицательное " + exc.salary);
             System.out.println("Введите новое значение зарплаты");
-            salary1=reader.readLine();
-            salary=Double.parseDouble(salary1);
+//            salary1=reader.readLine();
+//            salary=Double.parseDouble(salary1);
         }
+            catch (NumberFormatException exception){
+                System.out.println(exception.getMessage());
+            }
 
         }}
 }
